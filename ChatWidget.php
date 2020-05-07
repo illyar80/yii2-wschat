bd108ea8c25f0f9258bfa2fca6ec1a3b04e4cb37
napagokc <napagokc91@mail.ru>
2020-05-07 02:47:51 +0000

    user auth token

--- ChatWidget.php
+++ ChatWidget.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace jones\wschat;
 
 use Yii;
@@ -10,24 +11,23 @@ use yii\helpers\Json;
  * Class ChatWidget
  * @package jones\wschat
  */
-class ChatWidget extends Widget
-{
+class ChatWidget extends Widget {
     /**
      * @var boolean set to true if widget will be run for auth users
      */
     public $auth = false;
     public $user_id = null;
+    public $user_token = null;
     public $view = 'index';
     /** @var integer $port web socket port */
     public $port = 8080;
     /** @var array $chatList list of preloaded chats */
     public $chatList = [
-        'id' => 1,
+        'id'    => 1,
         'title' => 'All'
     ];
     /** @var string path to avatars folder */
     public $imgPath = '@vendor/joni-jones/yii2-wschat/assets/img';
-
     /**
      * @var boolean is user available to add nwe rooms
      */
@@ -36,14 +36,15 @@ class ChatWidget extends Widget
     /**
      * @override
      */
-    public function run()
-    {
+    public function run() {
         $this->registerJsOptions();
         Yii::$app->getAssetManager()->publish($this->imgPath);
-        return $this->render($this->view, [
-            'auth' => $this->auth,
+        return $this->render(
+            $this->view, [
+            'auth'     => $this->auth,
             'add_room' => $this->add_room
-        ]);
+        ]
+        );
     }
 
     /**
@@ -52,15 +53,15 @@ class ChatWidget extends Widget
      * @access protected
      * @return void
      */
-    protected function registerJsOptions()
-    {
+    protected function registerJsOptions() {
         $opts = [
-            'var currentUserId = '.($this->user_id ?: 0).';',
-            'var port = '.$this->port.';',
-            'var chatList = '.Json::encode($this->chatList).';',
-            'var imgPath = "'.Yii::$app->getAssetManager()->getPublishedUrl($this->imgPath).'";',
+            'var port = ' . $this->port . ';',
+            'var currentUserId = ' . ($this->user_id ?: 0) . ';',
+            'var chatToken = "' . ($this->user_token ?: 0) . '";',
+            'var chatList = ' . Json::encode($this->chatList) . ';',
+            'var imgPath = "' . Yii::$app->getAssetManager()->getPublishedUrl($this->imgPath) . '";',
         ];
-        $this->getView()->registerJs(implode(' ', $opts), View::POS_BEGIN);
+        $this->getView()->registerJs(PHP_EOL . implode(PHP_EOL, $opts), View::POS_BEGIN);
     }
 }
  
